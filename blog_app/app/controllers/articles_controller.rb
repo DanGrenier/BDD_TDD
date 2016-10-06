c00480f534712930a1c3c3519f00758e099cd24a
Daniel Grenier <dgrenier@smallbizpros.com>
2016-10-06 13:54:09 +0000

    Create article code

--- blog_app/app/controllers/articles_controller.rb
+++ blog_app/app/controllers/articles_controller.rb
@@ -1,4 +1,22 @@
 class ArticlesController < ApplicationController
   def index
   end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(articleParam)
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    end
+  end
+  
+  
+  private
+    def articleParam
+     params.require(:article).permit(:body,:title)
+    end
 end
