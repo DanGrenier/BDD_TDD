f9fdb3c6c480f6af6046f614b4cbebca9a6566c8
Daniel Grenier <dgrenier@smallbizpros.com>
2016-10-12 15:19:08 +0000

    Implement deleting article

--- blog_app/app/controllers/articles_controller.rb
+++ blog_app/app/controllers/articles_controller.rb
@@ -37,6 +37,15 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:success] = "Article has been deleted"
+      redirect_to articles_path
+    end
+      
+  end
+  
   
   private
     def articleParam
