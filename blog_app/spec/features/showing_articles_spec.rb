4d55b2d61c2db24d4beeeb25bf84d86cb1af92cb
Daniel Grenier <dgrenier@smallbizpros.com>
2016-10-14 20:06:35 +0000

    Restricted new edit and delete options

--- blog_app/spec/features/showing_articles_spec.rb
+++ blog_app/spec/features/showing_articles_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 RSpec.feature "Showing and Article" do
     before do
-        @article = Article.create(title: "The first article", body: "Body of the first article")
+        @john = User.create(email: "john@example.com", password: "password")
+        @fred = User.create(email: "fred@example.com", password: "password")
+        @article = Article.create(title: "The first article", body: "Body of the first article", user: @john)
+        
+        
+    end
+    
+    
+    scenario "A non-signed user does not see Edit or Delete" do
+        visit '/'
+        click_link @article.title
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(current_path).to eq(article_path(@article))
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+    end
+    
+    scenario "A non-owner signed in cannot see both links" do
+        login_as(@fred)
+        visit '/'
+        click_link @article.title
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+        
+    end
+    
+    scenario "A signed in owner can see both links" do 
+       login_as(@john) 
+       visit '/'
+        click_link @article.title
+        expect(page).to have_link("Edit Article")
+        expect(page).to have_link("Delete Article")
     end
     
     scenario "Display individual article" do
@@ -10,6 +42,7 @@ RSpec.feature "Showing and Article" do
         expect(page).to have_content(@article.title)
         expect(page).to have_content(@article.body)
         expect(current_path).to eq(article_path(@article))
+    
     end
     
     
